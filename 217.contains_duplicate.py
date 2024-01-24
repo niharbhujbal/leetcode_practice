811f2d7bc705b2abf75ac32922918674581d4421
Nihar Bhujbal <nihar.bhujbal@gmail.com>
2024-01-24 17:38:53 -0600

    solved two questions

---
+++ 217.contains_duplicate.py
@@ -0,0 +1,18 @@
+class Solution:
+    def containsDuplicate(self, nums) -> bool:
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        seen = set()
+        for i in nums:
+            if i in seen:
+                return True
+            else:
+                seen.add(i)
+        return False
+    
+# We will create a set
+# iterate over the list and if the element is in set then return True
+# else add that element into set
+# at the end if loop completes then there are no duplicates
---
+++ 242.valid_anagram.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        d = {}
+        for i in s:
+            if i in d.keys():
+                d[i] += 1
+            else:
+                d[i] = 1
+        
+        for i in t:
+            if i in d.keys():
+                if d[i] == 1:
+                    del d[i]
+                else:
+                    d[i] -= 1
+            else:
+                return False
+        return len(d) == 0:
+
+# we will create one hashmap
+# create letter as key and no of time that letter appears as a value
+# loop over first string and create the hashmap
+# now we will loop over the second string
+# if the letter is in dictonary then decrease the count of that letter by one and if the count goes to zero then delete the pair
+# if the key is not in the dictonary then retun false
+# at the end check if the dictonary is empty if true then it is a anagram else not an anagram
