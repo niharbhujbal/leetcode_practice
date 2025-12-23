e39b639feb8ed8efa2f007ecf5eda575c93ce4f8
niharbhujbal <60638991+niharbhujbal@users.noreply.github.com>
2025-12-23 17:24:32 -0600

    Time: 311 ms (5.10%) | Memory: 18.9 MB (82.04%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # calculate how many hours it would take to finish all the bananas if koko had spped of k
+        # koko_hours = sum([math.ceil(float(i) / k) for i in piles])
+        # binary search for h
+        left = 1
+        right = sum([math.ceil(float(i) / 1) for i in piles])
+        k = -1
+        while left <= right:
+            mid = left + (right - left) // 2
+            koko_hours = sum([math.ceil(float(i) / mid) for i in piles])
+            if koko_hours <= h:
+                k = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return k
+            
