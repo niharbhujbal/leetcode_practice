673a2eaeee9ca4797198398163ca56f05fe38cb5
niharbhujbal <60638991+niharbhujbal@users.noreply.github.com>
2025-12-23 23:00:29 -0600

    Time: 288 ms (12.89%) | Memory: 22.2 MB (15.83%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,29 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        # capcity low to high
+        # more days to less days
+        # 
+        # calculate days required based on capacity
+        def calculate_days(capacity):
+            days = 1
+            cargo = 0
+            for i in weights:
+                if cargo + i <= capacity:
+                    cargo += i
+                else:
+                    days += 1
+                    cargo = i
+            return days
+        
+        left = max(weights)
+        right = 500 * 5e4
+        ans = -1
+        while left <= right:
+            mid = left + (right - left) // 2
+            days_required = calculate_days(mid)
+            if days_required <= days:
+                ans = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return int(ans)
