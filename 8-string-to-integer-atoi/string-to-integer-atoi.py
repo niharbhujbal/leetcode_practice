fd3882fea80bb4c8e86f26092d234976cb525e1d
niharbhujbal <60638991+niharbhujbal@users.noreply.github.com>
2025-12-27 15:41:53 -0600

    Time: 4 ms (15.84%) | Memory: 17.3 MB (93.58%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,39 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        # skipping all the leading white spaces
+        ind = 0
+        while ind < len(s) and s[ind] == " ":
+            ind += 1
+        
+        # check leading sign
+        
+        if ind < len(s) and s[ind] == "-":
+            sign = -1
+            ind += 1
+        elif ind < len(s) and s[ind] == "+":
+            sign = 1
+            ind += 1
+        else:
+            sign = 1
+        
+        # then run is digit function and keep adding to number
+        value = 0
+        numbers = []
+        while ind < len(s) and s[ind].isdigit():
+            value = value * 10 + int(s[ind])
+            ind += 1
+        
+        # if passed invalid values
+        if value == 0:
+            return value
+
+        # to handle edge cases
+        INT_MIN = -(2 ** 31)
+        INT_MAX = (2 ** 31) - 1
+        signed_value = sign * value
+        if signed_value <= INT_MIN:
+            return INT_MIN
+        if signed_value >= INT_MAX:
+            return INT_MAX
+        
+        return signed_value
