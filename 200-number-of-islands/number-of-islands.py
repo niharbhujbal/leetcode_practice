421f5cfb4e0eb4f1bd46ff50343e895304832815
niharbhujbal <60638991+niharbhujbal@users.noreply.github.com>
2026-06-10 20:42:04 -0500

    Time: 239 ms (72.09%) | Memory: 21.7 MB (49.02%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,24 +1,21 @@
-from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
-
         islands = 0
-        for r in range(m):
-            for c in range(n):
-                if grid[r][c] == "1":
-                    islands += 1
-                    queue = deque()
-                    queue.append((r,c))
-                    grid[r][c] = "0"
 
-                    while queue:
-                        nr, nc = queue.popleft()
-                        for dr, dc in [(-1,0),(1,0),(0,-1),(0,1)]:
-                            nnr, nnc = nr+dr, nc + dc
-                            if 0 <= nnr < m and 0 <= nnc < n and grid[nnr][nnc] == "1":
-                                grid[nnr][nnc] = "0"
-                                queue.append((nnr, nnc))
-        
+        def dfs(i,j):
+            nonlocal m,n,grid
+            if i>= m or i < 0 or j >= n or j < 0 or grid[i][j] == '0':
+                return
+            directions = [(-1,0),(1,0),(0,-1),(0,1)]
+            grid[i][j] = '0'
+            for l,k in directions:
+                dfs(i+l, j+k)
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    islands += 1
+                    dfs(i,j)
         return islands
