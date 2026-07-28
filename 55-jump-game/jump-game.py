5d7a0507307bf976bd7c9c7fe7b0656e5e0bae73
niharbhujbal <60638991+niharbhujbal@users.noreply.github.com>
2026-07-27 22:11:53 -0500

    Time: 19 ms (61.86%) | Memory: 20.4 MB (29.43%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -2,7 +2,6 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         maxindx = 0
         for ind, ele in enumerate(nums):
-            print(maxindx, ind, ele)
             if maxindx < ind:
                 return False
             maxindx = max(maxindx, ind+ele)
